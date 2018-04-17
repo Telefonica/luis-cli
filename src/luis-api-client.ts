54c8dabed9252897682e4be57c1e44162cf7551b
Germán Toro del Valle <gtorodelvalle@gmail.com>
2018-04-17 10:46:48 +0200

    [[FIX]] Seting the default timeout value to 15000

--- src/luis-api-client.ts
+++ src/luis-api-client.ts
@@ -182,7 +182,7 @@ const LUIS_API_BASE_URL = 'https://westus.api.cognitive.microsoft.com';
 const RETRY_OPTS = {
     retries: 10,
     factor: 2,
-    minTimeout: 1500
+    minTimeout: 15000
 };
 // Maximum number of parallel requests per second to send to the API (to minimize 429 rate errors)
 const REQUESTS_PER_SECOND = 10;
@@ -211,7 +211,7 @@ export class LuisApiClient extends EventEmitter {
     constructor(config: LuisApiClientConfig) {
         super();
         this.config = config;
-        RETRY_OPTS.minTimeout = RETRY_OPTS.minTimeout || config.timeout;
+        RETRY_OPTS.minTimeout = config.timeout || RETRY_OPTS.minTimeout;
         let baseUrl = config.baseUrl || LUIS_API_BASE_URL;
         this.serviceReq = request.defaults({
             baseUrl: `${baseUrl}/luis/v2.0/apps/`,
